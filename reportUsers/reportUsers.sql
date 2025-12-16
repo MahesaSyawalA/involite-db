b574ee82c1469edf3bc4d0ea4f8a11f2a6e9af83
MahesaSyawalA <mahesaabd4@gmail.com>
2025-12-16 19:24:19 +0700

    adjustment : change label using camel case rules

--- reportUsers/reportUsers.sql
+++ reportUsers/reportUsers.sql
@@ -1,7 +1,7 @@
 -- Report Users table
 
 CREATE TABLE Reports(
-    reportID INT PRIMARY KEY AUTO_INCREMENT,
+    reportId INT PRIMARY KEY AUTO_INCREMENT,
     reportType ENUM('Violence', 'Harassment', 'Corruption', 'Other', 'Hate Speech', 'Fraud') NOT NULL,
     user VARCHAR(50) NOT NULL,
     reportDescription TEXT NOT NULL
