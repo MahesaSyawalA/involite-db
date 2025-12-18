6e1ef891ce27ebecbe46e9d7f6449704c1198d97
HilmiMithwa <hilmimithwa@gmail.com>
2025-12-18 23:33:04 +0700

    initialize reportUsers need

--- reportUsers/reportUsersTable.sql
+++ reportUsers/reportUsersTable.sql
@@ -1,3 +1,13 @@
+CREATE TABLE Reports(
+    reportID INT PRIMARY KEY AUTO_INCREMENT,
+    reportType ENUM('Violence', 'Harassment', 'Corruption', 'Other', 'Hate Speech', 'Fraud') NOT NULL,
+    user VARCHAR(50) NOT NULL,
+    reportDescription TEXT NOT NULL
+);
+
+SELECT @sql_mode;
+SET sql_mode = 'STRICT_TRANS_TABLES';
+
 INSERT INTO Reports (reportType, user, reportDescription) VALUES
 ('Fraud', 'Andi Kuliner', 'Penipuan pembayaran pesanan katering'),
 ('Harassment', 'Budi Coffee', 'Pelanggan mengirim pesan kasar kepada kasir'),
