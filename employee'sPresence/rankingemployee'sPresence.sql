32e8f70d77517d61cc61a64b1a1398de22d77ef8
HilmiMithwa <hilmimithwa@gmail.com>
2025-12-18 22:38:00 +0700

    adding ranking on presence

---
+++ employee'sPresence/rankingemployee'sPresence.sql
@@ -0,0 +1,10 @@
+-- Ranking untuk menampilkan kehadiran karyawan berdasarkan waktu absen masuk yang paling pagi 
+
+SELECT
+    userID,
+    presenceDate,
+    clockIN,
+    RANK() OVER (PARTITION BY presenceDate ORDER BY clockIN ASC) AS Masuk_Paling_Awal
+    FROM employeePresence
+
+
